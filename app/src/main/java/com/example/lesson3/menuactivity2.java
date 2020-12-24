57ddbd83c6bf48cb9ce902a40be9c36d0b32f96e
Azamat3E <aza.suindikovv@gmail.com>
2020-12-24 13:06:07 +0600

    commit

---
+++ app/src/main/java/com/example/lesson3/menuactivity2.java
@@ -0,0 +1,22 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menuactivity2  extends AppCompatActivity{
+
+
+
+    Button btnBack;
+
+
+    @Override
+    protected void onCreate (Bundle savedInctanceState) {
+        super.onCreate(savedInctanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+
+}
